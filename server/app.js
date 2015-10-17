2cfd79e5a134061190ad6c590af075005d39e834
Deathwing121 <kraus.martina.m@googlemail.com>
2015-10-17 17:26:50 +0200

    added server app.js

---
+++ server/app.js
@@ -0,0 +1,34 @@
+/**
+ * Created by makraus on 31/07/2015.
+ */
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var data = require('./data/data.json');
+var details = require('./data/detail.json');
+var _ = require('lodash');
+var router = express.Router();              // get an instance of the express Router
+
+router.get('/', function (req, res){
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+router.get('/people/:id', function (req, res){
+    var cat = _.find(details, { 'id': req.params.id});
+    res.json(cat);
+});
+
+router.get('/people', function (req, res){
+    res.json(data);
+});
+
+app.use('/api', router);
+app.use(express.static(__dirname + '/data'));
+
+
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
