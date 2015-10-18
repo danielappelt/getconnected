52a537f145f85a9569d0f2a763c96229c3c988e5
Daniel Appelt <daniel.appelt@gmail.com>
2015-10-19 01:43:49 +0200

    Connected the frontend with the backend API (2).

--- frontend/angular/indexApp.js
+++ frontend/angular/indexApp.js
@@ -7,9 +7,9 @@ angularModule.controller('MainGridCtrl', function($scope, $http){
     $scope.loadProfiles = function(){
         $scope.loading = true;
 
-        // currently defaults to mock
+        // Query people sorted by distance (TODO) from the server.
         $http({
-            url: "none",
+            url: "/api/people",
             method: "GET",
             withCredentials: false,
             headers: {
@@ -17,23 +17,23 @@ angularModule.controller('MainGridCtrl', function($scope, $http){
             }
         }).then(function(response){
             console.log(response);
-            $scope.users = response;
+            $scope.users = response.data;
         }).catch(function(response){
-            //wenn kein Service da ist, mock daten setzen
+            // Use mock data if the request is not successful.
             $scope.users = [
-                { name: "test0", preferences: ["glyphicon-book", "glyphicon-glass"], distance: 680, img: "http://www.american.edu/uploads/profiles/large/chris_palmer_profile_11.jpg"},
-                { name: "test5", preferences: ["glyphicon-glass"], distance: 200, img: "http://www.realtimearts.net/data/images/art/46/4640_profile_nilssonpolias.jpg"},
-                { name: "test1", preferences: ["glyphicon-glass"], distance: 150, img: "https://upload.wikimedia.org/wikipedia/commons/9/98/Christopher_Fabian_profile.jpg"},
-                { name: "test0", preferences: ["glyphicon-book", "glyphicon-glass"], distance: 680, img: "http://www.american.edu/uploads/profiles/large/chris_palmer_profile_11.jpg"},
-                { name: "test5", preferences: ["glyphicon-glass"], distance: 200, img: "http://www.realtimearts.net/data/images/art/46/4640_profile_nilssonpolias.jpg"},
-                { name: "test1", preferences: ["glyphicon-glass"], distance: 150, img: "https://upload.wikimedia.org/wikipedia/commons/9/98/Christopher_Fabian_profile.jpg"},
-                { name: "test0", preferences: ["glyphicon-book", "glyphicon-glass"], distance: 680, img: "http://www.american.edu/uploads/profiles/large/chris_palmer_profile_11.jpg"},
-                { name: "test5", preferences: ["glyphicon-glass"], distance: 200, img: "http://www.realtimearts.net/data/images/art/46/4640_profile_nilssonpolias.jpg"},
-                { name: "test1", preferences: ["glyphicon-glass"], distance: 150, img: "https://upload.wikimedia.org/wikipedia/commons/9/98/Christopher_Fabian_profile.jpg"},
-                { name: "test0", preferences: ["glyphicon-book", "glyphicon-glass"], distance: 680, img: "http://www.american.edu/uploads/profiles/large/chris_palmer_profile_11.jpg"},
-                { name: "test5", preferences: ["glyphicon-glass"], distance: 200, img: "http://www.realtimearts.net/data/images/art/46/4640_profile_nilssonpolias.jpg"},
-                { name: "test1", preferences: ["glyphicon-glass"], distance: 150, img: "https://upload.wikimedia.org/wikipedia/commons/9/98/Christopher_Fabian_profile.jpg"},
-                { name: "test4", preferences: ["glyphicon-flag", "glyphicon-glass"], distance: 800, img: "http://organicthemes.com/demo/profile/files/2012/12/profile_img.png"}
+                { nickname: "test0", hobbies: ["glyphicon-book", "glyphicon-glass"], distance: 680, image_url: "http://www.american.edu/uploads/profiles/large/chris_palmer_profile_11.jpg"},
+                { nickname: "test5", hobbies: ["glyphicon-glass"], distance: 200, image_url: "http://www.realtimearts.net/data/images/art/46/4640_profile_nilssonpolias.jpg"},
+                { nickname: "test1", hobbies: ["glyphicon-glass"], distance: 150, image_url: "https://upload.wikimedia.org/wikipedia/commons/9/98/Christopher_Fabian_profile.jpg"},
+                { nickname: "test0", hobbies: ["glyphicon-book", "glyphicon-glass"], distance: 680, image_url: "http://www.american.edu/uploads/profiles/large/chris_palmer_profile_11.jpg"},
+                { nickname: "test5", hobbies: ["glyphicon-glass"], distance: 200, image_url: "http://www.realtimearts.net/data/images/art/46/4640_profile_nilssonpolias.jpg"},
+                { nickname: "test1", hobbies: ["glyphicon-glass"], distance: 150, image_url: "https://upload.wikimedia.org/wikipedia/commons/9/98/Christopher_Fabian_profile.jpg"},
+                { nickname: "test0", hobbies: ["glyphicon-book", "glyphicon-glass"], distance: 680, image_url: "http://www.american.edu/uploads/profiles/large/chris_palmer_profile_11.jpg"},
+                { nickname: "test5", hobbies: ["glyphicon-glass"], distance: 200, image_url: "http://www.realtimearts.net/data/images/art/46/4640_profile_nilssonpolias.jpg"},
+                { nickname: "test1", hobbies: ["glyphicon-glass"], distance: 150, image_url: "https://upload.wikimedia.org/wikipedia/commons/9/98/Christopher_Fabian_profile.jpg"},
+                { nickname: "test0", hobbies: ["glyphicon-book", "glyphicon-glass"], distance: 680, image_url: "http://www.american.edu/uploads/profiles/large/chris_palmer_profile_11.jpg"},
+                { nickname: "test5", hobbies: ["glyphicon-glass"], distance: 200, image_url: "http://www.realtimearts.net/data/images/art/46/4640_profile_nilssonpolias.jpg"},
+                { nickname: "test1", hobbies: ["glyphicon-glass"], distance: 150, image_url: "https://upload.wikimedia.org/wikipedia/commons/9/98/Christopher_Fabian_profile.jpg"},
+                { nickname: "test4", hobbies: ["glyphicon-flag", "glyphicon-glass"], distance: 800, image_url: "http://organicthemes.com/demo/profile/files/2012/12/profile_img.png"}
             ];
             console.log("Fehler beim Abrufen der Daten, verwende Mock-Daten.");
         }).finally(function(){
